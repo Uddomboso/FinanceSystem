610d85c6b7b383568dc3295523498b8bd03e6547
Susan Mboso <susanmboso@gmail.com>
2025-07-08 16:35:18 -0700

    Update salary_checker.py

--- core/salary_checker.py
+++ core/salary_checker.py
@@ -1,6 +1,32 @@
-from datetime import datetime, timedelta
-from database.db_manager import fetch_one
+from database.db_manager import execute_query
+from datetime import datetime
 from core.commitment_manager import add_notification
+from database.db_manager import fetch_one
+
+
+def transfer_to_category(user_id, account_id, category_id, amount, note=""):
+    # log as expense in transactions
+    execute_query("""
+        INSERT INTO transactions (
+            user_id, account_id, category_id, amount, transaction_type, description, date
+        ) VALUES (?, ?, ?, ?, 'expense', ?, ?)
+    """, (user_id, account_id, category_id, amount, note or "Transfer to category", datetime.now()))
+
+    add_notification(user_id, f" You paid {amount} into category (ID: {category_id})")
+
+def transfer_to_savings(user_id, from_account_id, to_savings_account_id, amount, note="Transfer to savings"):
+    from database.db_manager import execute_query
+    from datetime import datetime
+
+    execute_query("""
+        INSERT INTO transactions (user_id, account_id, amount, transaction_type, description, date)
+        VALUES (?, ?, ?, 'expense', ?, ?)
+    """, (user_id, from_account_id, amount, note, datetime.now()))
+
+    execute_query("""
+        INSERT INTO transactions (user_id, account_id, amount, transaction_type, description, date)
+        VALUES (?, ?, ?, 'income', ?, ?)
+    """, (user_id, to_savings_account_id, amount, note, datetime.now()))
 
 def check_salary_reminder(user_id):
     today = datetime.now().day
